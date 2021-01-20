28db2229161e488b425a73e3e093aa8abe9a271a
Victor Premoli <victor.premoli57@gmail.com>
2021-01-19 21:59:33 -0300

    create user update form

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,16 +1,34 @@
 const User = require('../models/User')
+const { formatCep, formatCpfCnpj} = require("../../lib/utils")
 
 module.exports = {
     registerForm(req,res) {
+
+        
+
         return res.render("user/register")
     },
-    show(req,res) {
-        return res.send("cadastrado!!!!!")
+    async show(req,res) {
+
+        const { userId:id } = req.session
+        const user = await User.findOne({ where: {id} }) 
+
+        if(!user) return res.render("user/register", {
+            error: "Usuário não encontrado"
+        })
+
+        user.cpf_cnpj = formatCpfCnpj(user.cpf_cnpj)
+        user.cep = formatCep(user.cep)
+            
+        return res.render("user/index", { user })
     },
     async post(req, res) {
          
 
         const userId = await User.create(req.body)
+
+
+        req.session.userId = userId
         
         return res.redirect('/users')
     }

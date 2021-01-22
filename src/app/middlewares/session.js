0a8e5100ef50d922c84f2d1409e06ef130ccd934
Victor Premoli <victor.premoli57@gmail.com>
2021-01-22 19:35:23 -0300

    blocking and redirecting routes

---
+++ src/app/middlewares/session.js
@@ -0,0 +1,20 @@
+const session = require("express-session")
+
+function onlyUsers(req, res, next) {
+    if (!req.session.userId) {
+        return res.redirect('/users/login')
+    }
+    next()
+}
+
+function isLoggedRedirectToUsers(req, res, next) {
+    if(req.session.userId) {
+        return res.redirect('/users')
+    }
+    next()
+}
+
+module.exports = {
+    onlyUsers,
+    isLoggedRedirectToUsers
+}
